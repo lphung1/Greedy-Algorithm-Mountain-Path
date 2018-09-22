1908a86dbc75f5d27fd9aa9412ba4c1e5156fbca
lphung1 <loi_phung81@yahoo.com>
2018-09-21 21:44:03 -0400

    Updated algorithm to handle tie. Added ability to use another txt file and set dimentions of image in constructor

--- DrawMap.cpp
+++ DrawMap.cpp
@@ -11,11 +11,8 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-//#include "src/Bridges.h"
-//#include "src/Color.h"
-//#include "src/ColorGrid.h"
-//using namespace bridges;
-
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -24,7 +21,7 @@ class DrawMap
     
 private:
     int row, col;
-    int map[480][480];
+    int map[1000][1000]; //row, col
     string fileName = "Colorado_480x480.txt";
     int maxValue; //max value
     int minValue; //min value
@@ -37,65 +34,82 @@ private:
     
     
 public:
-    //default class constructor
-    DrawMap(){
+    /* default class constructor
+        string fname - file name
+        int r - row
+        int c - column
+        opens and reads file and puts into array and finds min and max values.
+        starts from lowest elevation initially
+     */
+    DrawMap(string fname, int r, int c){
         //initialize bridges
         //Bridges::initialize(3, "lphung1@uncc.edu", "234291986029"); //enter's api credentials
         //Bridges::setTitle("A Color Grid Example");
-        file.open(fileName);
         
-        cout << fileName << "\n" ;
+        row = r;
+        col = c;
         
-        if (file.is_open()){
-            cout << "File open\n";
-            getline(file, fileSize); //to skip first line
-            for (int i = 0; i < 480; i++) {
-                for (int j = 0; j < 480; j++) {
-                    file >> map[i][j];
+        if(row < 1000 || col < 1000){ //checks if dimentions are in bounds
+            fileName = fname;
+            
+            file.open(fileName);
+            
+            cout << fileName << "\n" ;
+            
+            if (file.is_open()){
+                cout << "File open\n";
+                getline(file, fileSize); //to skip first line. there is an extra 480 480 line in Colorado_480x480.txt file
+                for (int i = 0; i < row; i++) {
+                    for (int j = 0; j < col; j++) {
+                        file >> map[i][j];
+                    }
                 }
             }
-        }
-        else{
-        cout << "Issue opening file";
-        }
-        file.close();
-        //set default values for min/max
-        maxValue = map[0][0];
-        minValue = map [0][0];
-        //find min/max
-        findMinVal();
-        findMaxVal();
-        
-        currentValue = map[0][0];
-        currentColm = 0;
-        
-        //setting starting values
-        for(int i = currentColm; i < 480; i++){ //finds lowest value of first col
-            if(map[currentColm][i] < currentValue){
-                currentValue = map[currentColm][i];
-                indexOfMinColm = i; //assigns index of where lowest value is at
+            else{
+            cout << "Issue opening file";
             }
+            file.close();
+            
+            //set default values for min/max
+            maxValue = map[0][0];
+            minValue = map [0][0];
+            //find min/max
+            findMinVal();
+            findMaxVal();
+            
+            currentValue = map[0][0];
+            currentColm = 0;
+            
+            //setting starting values
+            for(int i = currentColm; i < row; i++){ //finds lowest value of first col
+                if(map[currentColm][i] < currentValue){
+                    currentValue = map[currentColm][i];
+                    indexOfMinColm = i; //assigns index of where lowest value is at
+                }
+            }
+        
+        }//end if statement
+        else{
+            cout << "Dimentions are out of bounds";
         }
-        
-        
         
     }//end constructor
     
     
     void showArray(){
         
-        for (int i = 0; i < 480; i++) {
-            for (int j = 0; j < 480; j++) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 cout << map[i][j] << " ";
             }
         }
     }//end showarray
     
     
-    
+    //finds lowest value in array
     void findMinVal(){
-        for (int i = 0; i < 480; i++) {
-            for (int j = 0; j < 480; j++) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 if (map[i][j] < minValue){
                     minValue = map[i][j];
                 }
@@ -104,10 +118,11 @@ public:
         
     }//end findMinVal
     
+    //finds highest value in array
     void findMaxVal(){
         
-        for (int i = 0; i < 480; i++) {
-            for (int j = 0; j < 480; j++) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 if (map[i][j] > maxValue){
                     maxValue = map[i][j];
                 }
@@ -141,6 +156,7 @@ public:
     }
     
     int getCurrentValueOfIndex(){
+        currentValue = map[indexOfMinColm][currentColm];
         return currentValue;
     }
     
@@ -149,6 +165,8 @@ public:
         
         int x1, x2, x3 = 0;
         int e1, e2, e3 = 0;
+        int random = 0;
+        //(rand() % 10) + 1
         
         x1 = map[currentColm + 1][indexOfMinColm + 1];
         x2 = map[currentColm + 1][indexOfMinColm];
@@ -159,29 +177,47 @@ public:
         e2 = abs(currentValue - x2);
         e3 = abs(currentValue - x3);
         
-        if(compareTo(e1, e2) == -1 && compareTo(e1, e3) == -1){ //go up
+        if(compareTo(e1, e2) == -1 && compareTo(e1, e3) == -1){ //go up if top is lowest num
             indexOfMinColm++;
             currentColm++;
-            currentValue = map[currentColm][indexOfMinColm];
         }
-        else if(compareTo(e2, e1) == -1 && compareTo(e2, e3) == -1){ //go straight
+        else if(compareTo(e2, e1) == -1 && compareTo(e2, e3) == -1){ //go straight if middle is lowest num
             currentColm++;
-            currentValue = map[currentColm][indexOfMinColm];
         }
-        else if(compareTo(e3, e2) == -1 && compareTo(e3, e1) == -1){ // go down
+        else if(compareTo(e3, e2) == -1 && compareTo(e3, e1) == -1){ // go down if bottom is lowest num
             indexOfMinColm--;
             currentColm++;
-            currentValue = map[currentColm][indexOfMinColm];
         }
-        else{
+        else if(compareTo(e1, e2) == 0){ //if top and middle are same and lowest num
+                currentColm++;
+                currentValue = map[currentColm][indexOfMinColm];
+        }
+        else if(compareTo(e3, e2) == 0){ //if bottom and middle are same and lowest num
+                currentColm++;
+                currentValue = map[currentColm][indexOfMinColm];
+        }
+        else if(compareTo(e3, e1) == 0){ //if bottom and top are same and lowest num, non straight tie so flip coin
+            srand(time(0));
+            random = (rand() % 2) + 1;
+            if(random == 1){//go down
+                indexOfMinColm--;
+                currentColm++;
+            }
+            else{//go up
+                indexOfMinColm++;
+                currentColm++;
+            }
+            
+        }
+        
+        else{ //failsafe, go straight
             currentColm++;
             currentValue = map[currentColm][indexOfMinColm];
         }
         
         
         
-        
-    }
+    }//end calc next path
     
     int drawNextPath(){
         
@@ -195,7 +231,25 @@ public:
         
     }
     
-private:
+    int getRow(){//return row size
+        return row;
+    }
+    
+    int getCol(){// return col size
+        return col;
+    }
+    void resetLine(){
+        currentColm = 0;
+            int random;
+            srand(time(0));
+            random = (rand() % row);
+            indexOfMinColm = random;
+        
+
+    }
+    
+    
+private: //compare numbers if lower or greater
     int compareTo(int x, int y){
         if (x > y){
             return 1;
@@ -206,6 +260,10 @@ private:
         else
             return 0;
     }
+    
+
+    
+
     
     
 };
--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 //
 //  main.cpp
-//  bridges_program
-//  prints random color squares on a red grid
-//  Created by Loi Phung on 8/29bri/18.
+//  Greedy algorithm mountain path program
+//  Created by Loi Phung on 9/14/2018
 //  Copyright © 2018 Loi Phung. All rights reserved.
 //
 
@@ -24,35 +23,74 @@ int main() {
     Bridges::initialize(3, "lphung1@uncc.edu", "234291986029"); //enter's api credentials
     Bridges::setTitle("Greedy Algorithm");
     
-    ColorGrid *cg  = new ColorGrid(480, 480, Color("white"));
-    
-    
-    // draw a green square
+    // call drawmap constructor and set colorgrid canvas
 
-    DrawMap map;
+    /*
+     set new file and canvas size here.
+     constructor will always start with lowest elevation in the first column
+     call map.startRandomRow() to reset and start from a random row
+     
+     will demonstrate algorithm with 3 examples below
+     */
+    DrawMap map("Colorado_480x480.txt", 480, 480);//file name, row, col
     
+    ColorGrid *cg  = new ColorGrid(map.getRow(), map.getCol(), Color("white"));
+    Bridges::setDataStructure(cg);
     
-    for (int i = 0; i < 480; i++){
-        for(int j = 0; j < 480; j++){
+    //print map
+    for (int i = 0; i < map.getRow(); i++){
+        for(int j = 0; j < map.getCol(); j++){
             int color = map.getColorOfArray(i, j);
             cg -> set(i, j, Color(color, color, color, 255));
         }
     }
-    
-    for(int i = 0; i < 480; i++){
+    //draw line from lowest elevation
+    for(int i = 0; i < map.getCol(); i++){
         map.calcNextPath();
         cg -> set(map.getIndexOfMinCol(), map.getCurrentCol(), Color("red"));
     }
+    Bridges::visualize();
     
+    map.resetLine();
+    //start from random point
+    for(int i = 0; i < map.getCol(); i++){
+        map.calcNextPath();
+        cg -> set(map.getIndexOfMinCol(), map.getCurrentCol(), Color("red"));
+    }
+    Bridges::visualize();
+    
+    //draw another from random point
+    map.resetLine();
+    for(int i = 0; i < map.getCol(); i++){
+        map.calcNextPath();
+        cg -> set(map.getIndexOfMinCol(), map.getCurrentCol(), Color("red"));
+    }
+    Bridges::visualize();
+    
+    /* comment out below to simulate the line multiple times
+    
+    int lines = 10;
+    for (int i = 0; i < lines; i++){
+        map.resetLine();
+        for(int i = 0; i < map.getCol(); i++){
+            map.calcNextPath();
+            cg -> set(map.getIndexOfMinCol(), map.getCurrentCol(), Color("red"));
+        }
+        Bridges::visualize();
+    }
+     */
+    
+    
+    //outputs of values for debug purposes
     cout <<"\nMax Value: " << map.getMaxVal() << "\nMin Value: " << map.getMinVal();
     cout <<"\nCurrent Col: " << map.getCurrentCol() << "\nIndexofMinCol: " << map.getIndexOfMinCol();
     cout << "\nCurrent Value: " << map.getCurrentValueOfIndex();
-    cout <<"\nScaled Min: " << map.scaleValue(map.getMinVal()) << "\nScaled Max: " <<map.scaleValue(map.getMaxVal());
+    cout <<"\nScaled Min: " << map.scaleValue(map.getMinVal()) << "\nScaled Max: " <<map.scaleValue(map.getMaxVal()) << "\n";
     
     
     
     
-    Bridges::setDataStructure(cg);
-    Bridges::visualize();
+    
+    
     return 0;
 }
